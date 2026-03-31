d7a328e010bff0955388319b0294cb9a07bf1db5
kjh11831 <k38140834@gmail.com>
2026-03-31 11:49:23 +0900

    2026.03.31.화

---
+++ 020_class, sturct/020_class.cs
@@ -0,0 +1,31 @@
+﻿namespace _020_class__sturct
+{
+
+    class Date
+    {
+        // 데이터(필드, 속성 = 변수)
+        int year, month, day;
+
+        // 동작(메소드 = 함수), 세터(setter), 게터(getter)
+        public void SetYear(int year)
+        {
+            this.year = year;
+        }
+
+        public int GetYear()
+        {
+            return this.year;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date birthday = new Date();
+            birthday.SetYear(2006);
+
+            Console.WriteLine("나는 {0}년생입니다.", birthday.GetYear());
+        }
+    }
+}
